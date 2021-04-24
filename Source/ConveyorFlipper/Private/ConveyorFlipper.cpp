73fd389afa5e731b37e189dfe3bebcc52c7b7bf0
DESKTOP-G1V79AR\jp <jeanpaul.rizkallah@gmail.com>
2021-04-24 22:13:50 +0200

    add hook on buildgun rightclick - flip normal conveyors

--- Source/ConveyorFlipper/Private/ConveyorFlipper.cpp
+++ Source/ConveyorFlipper/Private/ConveyorFlipper.cpp
@@ -2,16 +2,75 @@
 
 #include "ConveyorFlipper.h"
 
+#include "Buildables/FGBuildableConveyorBase.h"
+#include "Buildables/FGBuildableConveyorBelt.h"
+#include "Equipment/FGBuildGunDismantle.h"
+#include "Patching/NativeHookManager.h"
+#include "FGFactoryConnectionComponent.h"
+#include "Buildables/FGBuildableConveyorLift.h"
+
 DEFINE_LOG_CATEGORY(LogConveyorFlipperLog);
 
 IMPLEMENT_MODULE(FConveyorFlipperModule, ConveyorFlipper)
 
 #define LOCTEXT_NAMESPACE "FConveyorFlipperModule"
 
+FVector& FlipVector(FVector& Vector)
+{
+	Vector.X = -Vector.X;
+	Vector.Y = -Vector.Y;
+	return Vector;
+}
+
 void FConveyorFlipperModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	SUBSCRIBE_METHOD(UFGBuildGunStateDismantle::SecondaryFire,[] (CallScope<void(*)(UFGBuildGunState*)>& CallScope2, UFGBuildGunState* UfgBuildGunState)
+	{
+		bool bHit = false;
+		APlayerController* PlayerController = UfgBuildGunState->GetWorld()->GetFirstPlayerController();
+		FHitResult HitResult;
+		if (PlayerController)//r && LocalPlayer->ViewportClient)
+		{
+			//PlayerController->GetHitResultUnderCursor(ECC_GameTraceChannel6, false, HitResult);
+			//UE_LOG(LogConveyorFlipperLog, Warning, TEXT("bhit %s"), bHit? TEXT("true") : TEXT("false"));
+			{
+				
+				AActor* Actor = Cast<UFGBuildGunStateDismantle>(UfgBuildGunState)->GetSelectedActor();  //HitResult.GetActor();
+				if (Actor != NULL){
+					const FString ActorName  = Actor->GetName() ;
+					UE_LOG(LogConveyorFlipperLog, Warning, TEXT("The Actor's name is %s"), *ActorName);
 
+					if (Actor->GetClass()->IsChildOf(AFGBuildableConveyorBelt::StaticClass()))
+					{
+						//UE_LOG(LogCppTestModLog, Warning, TEXT("found conveyor!"));
+						AFGBuildableConveyorBelt* Conveyor = Cast<AFGBuildableConveyorBelt>(Actor);
+						UFGFactoryConnectionComponent* Connection0 = Conveyor->GetConnection0();
+						UFGFactoryConnectionComponent* Connection1 = Conveyor->GetConnection1();
+
+						if (!Connection0->IsConnected() && !Connection1->IsConnected())
+						{
+							TArray< FSplinePointData > OldSplineData = Conveyor->GetSplineData();
+							const uint32 ArraySize = OldSplineData.Num();
+							TArray< FSplinePointData > NewSplineData;
+							//UE_LOG(LogCppTestModLog, Warning, TEXT("array size %d"), ArraySize);
+
+							for(uint32 i= ArraySize - 1 ; i > 0 ; i--)
+							{
+								NewSplineData.Emplace(FSplinePointData(OldSplineData[i].Location, FlipVector(OldSplineData[i].LeaveTangent), FlipVector(OldSplineData[i].ArriveTangent)));
+							}
+							NewSplineData.Emplace(FSplinePointData(OldSplineData[0].Location, FlipVector(OldSplineData[0].LeaveTangent), FlipVector(OldSplineData[0].ArriveTangent)));
+							AFGBuildableConveyorBelt::Respline(Conveyor, NewSplineData);
+						}
+					}
+
+					
+				}
+			}
+		}
+		//return bHit;
+		
+	});     		
 }
 
 void FConveyorFlipperModule::ShutdownModule()
@@ -19,6 +78,7 @@ void FConveyorFlipperModule::ShutdownModule()
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 }
+
 
 
 #undef LOCTEXT_NAMESPACE
