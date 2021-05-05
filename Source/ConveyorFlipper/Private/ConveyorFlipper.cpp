d611b89af3bf1050cbee58f4679476e454f25294
DESKTOP-G1V79AR\jp <jeanpaul.rizkallah@gmail.com>
2021-05-05 22:34:24 +0200

    refactor belt flip

--- Source/ConveyorFlipper/Private/ConveyorFlipper.cpp
+++ Source/ConveyorFlipper/Private/ConveyorFlipper.cpp
@@ -2,12 +2,19 @@
 
 #include "ConveyorFlipper.h"
 
+#include "AFGMyConveyorLift.h"
+#include "FGColoredInstanceMeshProxy.h"
 #include "Buildables/FGBuildableConveyorBase.h"
 #include "Buildables/FGBuildableConveyorBelt.h"
 #include "Equipment/FGBuildGunDismantle.h"
 #include "Patching/NativeHookManager.h"
 #include "FGFactoryConnectionComponent.h"
 #include "Buildables/FGBuildableConveyorLift.h"
+#include "InputCoreTypes.h"
+#include "Registry/ModKeyBindRegistry.h"
+#include "Algo/Reverse.h"
+#include "Equipment/FGBuildGunBuild.h"
+#include "Hologram/FGConveyorLiftHologram.h"
 
 DEFINE_LOG_CATEGORY(LogConveyorFlipperLog);
 
@@ -22,49 +29,50 @@ FVector& FlipVector(FVector& Vector)
 	return Vector;
 }
 
+
+
 void FConveyorFlipperModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	SUBSCRIBE_METHOD(UFGBuildGunStateDismantle::SecondaryFire,[] (CallScope<void(*)(UFGBuildGunState*)>& CallScope2, UFGBuildGunState* UfgBuildGunState)
 	{
-		bool bHit = false;
+	//	UModKeyBindRegistry::RegisterModKeyBind("ConveyorFlipper", FInputActionKeyMapping("ConveyorFlipper.FlipConveyor","E"),FText::FromString("Conveyor Flipper"));
 		APlayerController* PlayerController = UfgBuildGunState->GetWorld()->GetFirstPlayerController();
 		FHitResult HitResult;
 		if (PlayerController)//r && LocalPlayer->ViewportClient)
 		{
-			//PlayerController->GetHitResultUnderCursor(ECC_GameTraceChannel6, false, HitResult);
-			//UE_LOG(LogConveyorFlipperLog, Warning, TEXT("bhit %s"), bHit? TEXT("true") : TEXT("false"));
 			{
-				
-				AActor* Actor = Cast<UFGBuildGunStateDismantle>(UfgBuildGunState)->GetSelectedActor();  //HitResult.GetActor();
-				if (Actor != NULL){
-					const FString ActorName  = Actor->GetName() ;
-					UE_LOG(LogConveyorFlipperLog, Warning, TEXT("The Actor's name is %s"), *ActorName);
+				if (UfgBuildGunState->GetBuildGun()->IsInState(EBuildGunState::BGS_DISMANTLE)){
+					AActor* Actor = Cast<UFGBuildGunStateDismantle>(UfgBuildGunState)->GetSelectedActor();  //HitResult.GetActor();
+					if (Actor != NULL && Actor != nullptr){
+						const FString ActorName  = Actor->GetName();
+						UE_LOG(LogConveyorFlipperLog, Warning, TEXT("The Actor's name is %s"), *ActorName);
 
-					if (Actor->GetClass()->IsChildOf(AFGBuildableConveyorBelt::StaticClass()))
-					{
-						//UE_LOG(LogCppTestModLog, Warning, TEXT("found conveyor!"));
-						AFGBuildableConveyorBelt* Conveyor = Cast<AFGBuildableConveyorBelt>(Actor);
-						UFGFactoryConnectionComponent* Connection0 = Conveyor->GetConnection0();
-						UFGFactoryConnectionComponent* Connection1 = Conveyor->GetConnection1();
-
-						if (!Connection0->IsConnected() && !Connection1->IsConnected())
+						if (Actor->GetClass()->IsChildOf(AFGBuildableConveyorBelt::StaticClass()))
 						{
-							TArray< FSplinePointData > OldSplineData = Conveyor->GetSplineData();
-							const uint32 ArraySize = OldSplineData.Num();
-							TArray< FSplinePointData > NewSplineData;
-							//UE_LOG(LogCppTestModLog, Warning, TEXT("array size %d"), ArraySize);
+							//UE_LOG(LogCppTestModLog, Warning, TEXT("found conveyor!"));
+							AFGBuildableConveyorBelt* Conveyor = Cast<AFGBuildableConveyorBelt>(Actor);
+							UFGFactoryConnectionComponent* Connection0 = Conveyor->GetConnection0();
+							UFGFactoryConnectionComponent* Connection1 = Conveyor->GetConnection1();
 
-							for(uint32 i= ArraySize - 1 ; i > 0 ; i--)
+							if (!Connection0->IsConnected() && !Connection1->IsConnected())
 							{
-								NewSplineData.Emplace(FSplinePointData(OldSplineData[i].Location, FlipVector(OldSplineData[i].LeaveTangent), FlipVector(OldSplineData[i].ArriveTangent)));
-							}
-							NewSplineData.Emplace(FSplinePointData(OldSplineData[0].Location, FlipVector(OldSplineData[0].LeaveTangent), FlipVector(OldSplineData[0].ArriveTangent)));
-							AFGBuildableConveyorBelt::Respline(Conveyor, NewSplineData);
-						}
-					}
+								TArray< FSplinePointData > OldSplineData = Conveyor->GetSplineData();
+								const uint32 ArraySize = OldSplineData.Num();
+								TArray< FSplinePointData > NewSplineData;
+								//UE_LOG(LogCppTestModLog, Warning, TEXT("array size %d"), ArraySize);
 
-					
+								for(uint32 i= ArraySize - 1 ; i > 0 ; i--)
+								{
+									NewSplineData.Add(FSplinePointData(OldSplineData[i].Location, FlipVector(OldSplineData[i].LeaveTangent), FlipVector(OldSplineData[i].ArriveTangent)));
+								}
+								NewSplineData.Add(FSplinePointData(OldSplineData[0].Location, FlipVector(OldSplineData[0].LeaveTangent), FlipVector(OldSplineData[0].ArriveTangent)));
+								AFGBuildableConveyorBelt::Respline(Conveyor, NewSplineData);
+							}
+						}
+
+						
+					}
 				}
 			}
 		}
